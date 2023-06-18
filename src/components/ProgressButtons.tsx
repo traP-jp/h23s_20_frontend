3c633653da1c6cd7e10dd321f750c7e75527e4f7
Apple000001 <85782741+Apple000001@users.noreply.github.com>
2023-06-18 02:54:38 +0000

    進捗ボタンの見た目を変更

--- src/components/ProgressButtons.tsx
+++ src/components/ProgressButtons.tsx
@@ -1,11 +1,11 @@
 import axios from 'axios'
 import React, { useState } from 'react'
-
 import { PointType, Progress as ProgressType } from '@/types/progress'
-
 import { getApiOrigin } from '../../env'
-
-import styles from './ProgressButtons.module.css'
+import { FaGrinAlt,FaGrinSquint,FaGrinStars} from 'react-icons/fa';
+import {TbNumber1,TbNumber3,TbNumber5} from 'react-icons/tb';
+import { Button, IconButton, Pane, PlusIcon } from 'evergreen-ui'
+import { IconContext } from 'react-icons'
 
 export default function ProgressButtons() {
 	const [hideButton, setHideButton] = useState(false)
@@ -22,20 +22,49 @@ export default function ProgressButtons() {
 
 	return (
 		<div>
-			<button className={styles.plusbutton} onClick={toggle}>
-				+
-			</button>
+			<Pane position="fixed" bottom="50px" right="50px">
+				<IconButton icon={PlusIcon} onClick={toggle} height={60}/>
+			</Pane>
+			
 			{hideButton && (
 				<div>
-					<button className={styles.button1} onClick={() => buttonPush('one')}>
-						👍
-					</button>
-					<button className={styles.button2} onClick={() => buttonPush('three')}>
-						👍
-					</button>
-					<button className={styles.button3} onClick={() => buttonPush('five')}>
-						👍
-					</button>
+					<Pane position="fixed" bottom="50px" right="200px">
+						<Button height={60}  width={60} onClick={() => buttonPush('one')}>
+							<Pane>
+								<Pane position="fixed" bottom="50px" right="200px">
+									<div><TbNumber1 size="13px" /></div>
+								</Pane>
+								<IconContext.Provider value={{size: '30px' }}>
+									<div><FaGrinAlt/></div>
+								</IconContext.Provider>
+								
+							</Pane>
+						</Button>
+					</Pane>
+					<Pane position="fixed" bottom="140px" right="140px">
+						<Button height={60} width={60} onClick={() => buttonPush('three')}>
+							<Pane >
+								<IconContext.Provider value={{size: '30px'}}>
+									<div><FaGrinSquint/></div>
+								</IconContext.Provider>
+								<Pane position="fixed" bottom="140px" right="140px">
+									<div><TbNumber3 size="13px"/></div>
+								</Pane>
+							</Pane>
+						</Button>
+					</Pane>
+					<Pane position="fixed" bottom="200px" right="50px">
+						<Button height={60} width={60} onClick={() => buttonPush('five')}>
+							<Pane>
+								<IconContext.Provider value={{size: '30px'}}>
+									<div><FaGrinStars/></div>
+								</IconContext.Provider>
+								<Pane position="fixed" bottom="200px" right="50px">
+									<div><TbNumber5 size="13px"/></div>
+								</Pane>
+							</Pane>
+						</Button>
+					</Pane>
 				</div>
 			)}
 		</div>
